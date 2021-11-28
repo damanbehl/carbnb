6bf74ef243e5aaae5132d087d9880a585288ffca
panguluriarvind <panguluriarvind@gmail.com>
2021-11-27 22:07:59 -0500

    first screen added

--- lib/main.dart
+++ lib/main.dart
@@ -18,54 +18,68 @@ class _carbnbState extends State<carbnb> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.black38,
+        backgroundColor: Color(0xff021127),
         body: SafeArea(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-
-              Center(
-                child: Container(decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20.0),color: Colors.blue,),height: 50,width: 275,
-                    margin: EdgeInsets.fromLTRB(0.0, 170.0, 0.0, 0.0),
-                    child: Center(child: ElevatedButton(onPressed: (){print('hello');},
-                        style: ElevatedButton.styleFrom(fixedSize: Size(275, 50)),
-
-                        child: Text('Login',style: TextStyle(
-                          fontSize: 20,color: Colors.white,letterSpacing: 2,),
-                        )))),
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('images/287755-grayscale-style 1.png'),
+              fit: BoxFit.cover,
               ),
-              Container(
-                  margin: EdgeInsets.all(35.0),
-                  color: Color(0xFF2A2E32),
-                  child: ElevatedButton(onPressed: (){}, child:Text('Sign Up',style: TextStyle(fontSize: 20,letterSpacing: 2)),
-                    style:ElevatedButton.styleFrom(fixedSize: Size(275.0, 50.0),primary: Color(0xff272A2E)),)
+            ),
+            child: Column(mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Center(
+                  child: Text('Car Booking',style: TextStyle(
+                    fontSize: 50,color: Colors.white
+                  ), ),
+                ),
+
+                Center(
+                  child: Container(decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20.0),color: Colors.blue,),height: 50,width: 275,
+                      margin: EdgeInsets.fromLTRB(0.0, 400.0, 0.0, 20.0),
+                      child: Center(child: ElevatedButton(onPressed: (){print('hello');},
+                          style: ElevatedButton.styleFrom(fixedSize: Size(275, 50)),
+
+                          child: Text('Login',style: TextStyle(
+                            fontSize: 20,color: Colors.white,letterSpacing: 2,),
+                          )))),
+                ),
+                Container(
+                    margin: EdgeInsets.fromLTRB(35, 20, 35, 50),
+                    color: Color(0xFF2A2E32),
+                    child: ElevatedButton(onPressed: (){}, child:Text('Sign Up',style: TextStyle(fontSize: 20,letterSpacing: 2)),
+                      style:ElevatedButton.styleFrom(fixedSize: Size(275.0, 50.0),primary: Color(0xff272A2E)),)
 
 
-              ),
-              Divider(
+                ),
+                Divider(
 
-                height: 5,
-                color: Colors.white,
-                thickness: 0,
-                indent: 40,
-                endIndent: 40,
-
-
-              ),
-
-              Container(
-                  margin: EdgeInsets.all(35.0),
-
-                  child: ElevatedButton(onPressed: (){print('facebook');}, child:
-                  RichText( text: TextSpan(text: 'SignIn with  ', style: TextStyle(color: Colors.black,fontSize: 20),
-                  children: const<TextSpan>[
-                    TextSpan(text: 'facebook',style: TextStyle(color: Colors.blue,fontWeight: FontWeight.bold)),
-                  ]),),
-                    style:ElevatedButton.styleFrom(fixedSize: Size(275.0, 50.0),primary: Color(0xffF4F8FC),)
+                  height: 5,
+                  color: Colors.white,
+                  thickness: 0,
+                  indent: 40,
+                  endIndent: 40,
 
 
-              ),
-              ),],
+                ),
+
+                Container(
+
+                    margin: EdgeInsets.fromLTRB(35, 70, 35, 0),
+
+                    child: ElevatedButton(onPressed: (){print('facebook');}, child:
+                    RichText( text: TextSpan(text: 'SignIn with  ', style: TextStyle(color: Colors.black,fontSize: 20),
+                    children: const<TextSpan>[
+                      TextSpan(text: 'facebook',style: TextStyle(color: Colors.blue,fontWeight: FontWeight.bold)),
+                    ]),),
+                      style:ElevatedButton.styleFrom(fixedSize: Size(275.0, 50.0),primary: Color(0xffF4F8FC),)
+
+
+                ),
+                ),],
+            ),
           ),
         ),
       ),
