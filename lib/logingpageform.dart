e7760e62e17d86299ed55805dc27feac46688840
panguluriarvind <panguluriarvind@gmail.com>
2021-12-02 22:56:17 -0500

    signup page created

--- lib/logingpageform.dart
+++ lib/logingpageform.dart
@@ -11,27 +11,70 @@ class _logingpageformState extends State<logingpageform> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(appBarTheme:AppBarTheme(backgroundColor: Color(0xff1A1F24)),
+      theme: ThemeData(appBarTheme:AppBarTheme(backgroundColor: Color(
+          0xff021127)),
       ),
       home: Scaffold(
-        backgroundColor: Color(0xff021127),
+        backgroundColor: Colors.grey,
 
         appBar: AppBar(
-          title: Text('Regist'),
+          title: Text('Registration'),
         ),
 
         body: Column(crossAxisAlignment: CrossAxisAlignment.center,mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Container(
-                child: TextFormField(
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'User Name',
-                      hintText: 'Enter valid mail id as abc@gmail.com'
-                  ),
-                ),
-
+              child: Text('Welcome,     '
+                  ,style: TextStyle(fontSize: 60),),
             ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+
+                cursorColor: Colors.white,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Name',
+                    hintText: ' Last Name'
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+
+                cursorColor: Colors.white,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'User Name',
+                    hintText: 'Enter valid mail id as abc@gmail.com'
+                ),
+              ),
+            ),
+      Padding(
+        padding: EdgeInsets.all(10),
+        child: TextField(
+          obscureText: true,
+          decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Password',
+              hintText: 'Enter your secure password'
+          ),
+        ),
+      ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Confirm Password',
+                    hintText: 'Enter your secure password'
+                ),
+              ),
+            ),
+            Container(child: ElevatedButton(onPressed: () { print('submit'); }, child: Text('submit'),),)
           ],
         ),
       ),);
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,27 @@
 import 'package:flutter/material.dart';
 import 'login_splash.dart';
+import 'logingpageform.dart';
+
+
+import 'package:carbnb/page1.dart';
+import 'logingpageform.dart';
 
 void main() {
-  runApp(carbnb());
+  runApp(logingpageform());
 }
+
+// class carbnb extends StatefulWidget {
+//   const carbnb({Key? key}) : super(key: key);
+//
+//   @override
+//   _carbnbState createState() => _carbnbState();
+// }
+//
+// class _carbnbState extends State<carbnb> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return logingpageform();
+//   }
+// }
+
 
