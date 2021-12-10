75bb1abb94fff5e9d7949e0ac7761feb5488d1e3
panguluriarvind <panguluriarvind@gmail.com>
2021-12-10 10:49:18 -0500

    sign up page form

--- lib/logingpageform.dart
+++ lib/logingpageform.dart
@@ -10,73 +10,76 @@ class logingpageform extends StatefulWidget {
 class _logingpageformState extends State<logingpageform> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(appBarTheme:AppBarTheme(backgroundColor: Color(
-          0xff021127)),
+    return Scaffold(
+      // resizeToAvoidBottomInset: true,
+
+      backgroundColor: Colors.grey,
+      appBar: AppBar(
+        title: const Text('Registration'),
       ),
-      home: Scaffold(
-        backgroundColor: Colors.grey,
-
-        appBar: AppBar(
-          title: Text('Registration'),
-        ),
-
-        body: Column(crossAxisAlignment: CrossAxisAlignment.center,mainAxisAlignment: MainAxisAlignment.center,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Container(
-              child: Text('Welcome,     '
-                  ,style: TextStyle(fontSize: 60),),
+              child: const Text(
+                'Welcome,     ',
+                style: TextStyle(fontSize: 60),
+              ),
             ),
-            Padding(
+            const Padding(
               padding: EdgeInsets.all(10),
               child: TextField(
-
                 cursorColor: Colors.white,
                 decoration: InputDecoration(
                     border: OutlineInputBorder(),
                     labelText: 'Name',
-                    hintText: ' Last Name'
-                ),
+                    hintText: ' Last Name'),
               ),
             ),
-
-            Padding(
+            const Padding(
               padding: EdgeInsets.all(10),
               child: TextField(
-
                 cursorColor: Colors.white,
                 decoration: InputDecoration(
                     border: OutlineInputBorder(),
                     labelText: 'User Name',
-                    hintText: 'Enter valid mail id as abc@gmail.com'
-                ),
+                    hintText: 'Enter valid mail id as abc@gmail.com'),
               ),
             ),
-      Padding(
-        padding: EdgeInsets.all(10),
-        child: TextField(
-          obscureText: true,
-          decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Password',
-              hintText: 'Enter your secure password'
-          ),
-        ),
-      ),
-            Padding(
+            const Padding(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                    hintText: 'Enter your secure password'),
+              ),
+            ),
+            const Padding(
               padding: EdgeInsets.all(10),
               child: TextField(
                 obscureText: true,
                 decoration: InputDecoration(
                     border: OutlineInputBorder(),
                     labelText: 'Confirm Password',
-                    hintText: 'Enter your secure password'
-                ),
+                    hintText: 'Enter your secure password'),
               ),
             ),
-            Container(child: ElevatedButton(onPressed: () { print('submit'); }, child: Text('submit'),),)
+            // Container(
+            //   child:
+            ElevatedButton(
+              onPressed: () {
+                print('submit');
+              },
+              child: const Text('submit'),
+            ),
+            // )
           ],
         ),
-      ),);
+      ),
+    );
   }
 }
