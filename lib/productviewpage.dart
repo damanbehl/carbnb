c2cf5703f5f7e4a8abc6b71a0742d9316c7239ac
panguluriarvind <panguluriarvind@gmail.com>
2021-12-17 19:35:55 -0500

    pubspec fixed

--- lib/productviewpage.dart
+++ lib/productviewpage.dart
@@ -13,27 +13,16 @@ class productviewpage extends StatefulWidget {
 class _productviewpageState extends State<productviewpage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child:Column(
-          children: [
-
-            Container(
-              height: MediaQuery.of(context).size.height,
-              width: MediaQuery.of(context).size.width,
-              color: defaultColor,
-              child: Image(
-                image: AssetImage(
-                  'assets/images/BlackForest-Germany-GettyImages-147180370.jpeg',
-                ),
-              ),
-            ),
-
-
-          ],
-        ),
-      ),
-
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        Positioned(
+            left: 20,
+            right: 20,
+            top: 20,
+            child: Image.asset("assets/images/BlackForest-Germany-GettyImages-147180370.jpeg"))
+      ],
     );
   }
 }
+
